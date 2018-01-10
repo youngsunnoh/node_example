821489c6d79a8f613daf3b1a902707cb2ad52508
youngsunnoh <eigrp100@naver.com>
2018-01-10 17:38:12 +0900

    nodejs 테스트 ( morgan.logger 종류 기입 )

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger());
+app.use(logger('combined')); // combined,common,dev,short,tiny
 /*app.use(logger('dev'));*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
