f3b5e51c5283114ae28f6e2a305a7324b7fdaaf1
youngsunnoh <eigrp100@naver.com>
2018-01-31 18:13:31 +0900

    nodejs 테스트 ( 로그프레소 연동 및 connection 분리 )

--- routes/logpresso.js
+++ routes/logpresso.js
@@ -6,20 +6,26 @@
 // npm install --global --production windows-build-tools
 // JDK 1.7 요구사항
 // PATH 설정 : jdk1.7/bin , jre7/bin
+// jar 파일은 config에서  import 
+// config 에서 connection 생성 
 
 var express = require('express');
 var router = express.Router();
 var java = require("java");
+// SDK 연동
 var logpresso = require('../config/logpresso-connection');
 var javaLangSystem = java.import('java.lang.System');
 
-//쿼리 조회
+// 쿼리 조회 ( 로그프레소에서 제공하는 java sdk 연결 )
+// Class 메소드 호출 시 메소드 이름뒤에 Sync 처리
 var connection = logpresso.getJavaInstance();
 var cursor = connection.querySync("table limit=100 dev_spider_data");
 //임의의 오브젝트 생성
 var obj = {table: []};
+
 while(cursor.hasNextSync()){
     var Tuple = cursor.nextSync();
+    //로그프레소 결과는 튜플 형태로 res
     obj.table.push(Tuple.getSync("author"));
     obj.table.push(Tuple.getSync("biz_kind"));
     obj.table.push(Tuple.getSync("postdate"));
